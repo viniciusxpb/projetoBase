e62f1f35671ecbf68074196cfdd2e5004c99f785
viniciusxpb <vinicius_franca@hotmail.com.br>
2024-08-30 17:53:44 -0300

    usando objetos

--- src/app/components/componente10/componente10.component.ts
+++ src/app/components/componente10/componente10.component.ts
@@ -8,6 +8,30 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class Componente10Component {
   @Output() clicked = new EventEmitter<void>();
 
+  listaPessoas =[
+    {
+      nome:"João",
+      comidaFavorita: "Pizza"
+    },
+    {
+      nome:"Maria",
+      comidaFavorita: "Chocolate"
+    },
+    {
+      nome:"Pedro",
+      comidaFavorita: "Banana"
+    },
+    {
+      nome:"Jorge",
+      comidaFavorita: "Pastel"
+    },
+    
+  ]
+
+  cliqueiBotaoFor(pessoa:any){
+    window.alert("A comida favorita de"+pessoa.nome+" é "+pessoa.comidaFavorita);
+  }
+
   onClick() {
     this.clicked.emit();
   }
@@ -20,3 +44,4 @@ export class Componente10Component {
 
 
 }
+
